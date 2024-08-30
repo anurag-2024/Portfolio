2852b3808c4ab572ee4633c169c3b288ecc9640c
anurag-2420 <anuragpatel20june@gmail.com>
2024-08-31 01:39:35 +0530

    update

--- client/src/utils/work.js
+++ client/src/utils/work.js
@@ -1,16 +1,29 @@
 import img1 from '../assets/carrer/carrer-1.webp'
 import img2 from '../assets/carrer/carrer-2.png'
+import img3 from '../assets/carrer/carrer-3.png'
 
 export const work = [
     {
-        date: 'Apr 2024- Present',
+        date: 'July 2024- Present',
+        icon: img3,
+        title: 'Backend Developer',
+        company: 'Bista Technologies Inc.',
+        description: [
+            'Engineered scalable backend architecture using Node.js, Express, and MongoDB for the platform.',
+            'Integrated third-party email services for automated user notifications and communications.',
+            'Developed and optimized RESTful APIs for seamless frontend-backend integration.',
+            'Collaborated with cross-functional teams to ensure efficient deployment and system integration.',
+        ]
+    },
+    {
+        date: 'Apr 2024- Aug 2024',
         icon: img1,
         title: 'Backend Developer',
         company: 'IIIT Una',
         description: [
-        'Developed backend APIs using Node.js to support faculty and admin portals.',
-        'Implemented secure authentication for faculty and admin portals.',
-        'Enhanced interactivity of the website through well-structured APIs.'
+            'Developed backend APIs using Node.js to support faculty and admin portals.',
+            'Implemented secure authentication for faculty and admin portals.',
+            'Enhanced interactivity of the website through well-structured APIs.'
         ]
     },
     {
@@ -19,10 +32,10 @@ export const work = [
         title: 'Backend Developer',
         company: 'SharpCarrer Technologies',
         description: [
-        'Led a team of 5 members to successfully complete a full-stack project.',
-        'Primarily focused on backend development, while also contributing to frontend tasks.',
-        'Developed a responsive website with secure admin login for room status updates.',
-        'Created and managed APIs, leveraging Context API for global state management.'
+            'Led a team of 5 members to successfully complete a full-stack project.',
+            'Primarily focused on backend development, while also contributing to frontend tasks.',
+            'Developed a responsive website with secure admin login for room status updates.',
+            'Created and managed APIs, leveraging Context API for global state management.'
         ]
     }
 ]
